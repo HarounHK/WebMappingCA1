0530ca7ac51fbf204dd39481ee4f9e9cfd29c10e
Haroun Kassouri <harounkassouri368@gmail.com>
2024-11-10 18:27:50 +0000

    Lab1

--- awmproject/awmproject/urls.py
+++ awmproject/awmproject/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# awmproject/urls.py
 from django.contrib import admin
 from django.urls import path
+from map import views  # Import views from the map app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('map/', views.map_view, name='map'),  # /map/ URL for map view
 ]
--- awmproject/testlib.py
+++ awmproject/testlib.py
@@ -1,23 +1,4 @@
-try:
-    import django
-    print("Django imported successfully")
-except ImportError as e:
-    print(f"Failed to import Django: {e}")
-
-try:
-    from osgeo import gdal
-    print("GDAL imported successfully")
-except ImportError as e:
-    print(f"Failed to import GDAL: {e}")
-
-try:
-    from pyproj import Proj
-    print("PyProj imported successfully")
-except ImportError as e:
-    print(f"Failed to import PyProj: {e}")
-
-try:
-    import psycopg2
-    print("Psycopg2 imported successfully")
-except ImportError as e:
-    print(f"Failed to import Psycopg2: {e}")
+import django
+from osgeo import gdal
+from pyproj import Proj
+import psycopg2
