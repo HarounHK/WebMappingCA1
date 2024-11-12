8de0e2a641f9f249391786038b017c9b76f65d98
Haroun Kassouri <harounkassouri368@gmail.com>
2024-11-12 02:33:28 +0000

    added search functionality temporary

--- geodjango/map/urls.py
+++ geodjango/map/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'map'  
+app_name = 'map'
 
 urlpatterns = [
     path('map/', views.map_view, name='map'),
+    path('add_location/', views.add_location, name='add_location'),
 ]
--- geodjango/map/views.py
+++ geodjango/map/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Location
+from django.db.models import Q  # For search functionality
 
-# Create your views here.
+# Map view to display locations
 def map_view(request):
-    locations = Location.objects.all()
+    query = request.GET.get('query')  # Get the search query from the form
+    if query:
+        locations = Location.objects.filter(
+            Q(name__icontains=query) | Q(address__icontains=query)
+        )
+    else:
+        locations = Location.objects.all()
+
     return render(request, 'map.html', {'locations': locations})
+
+# View to add a new location
+def add_location(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        address = request.POST['address']
+        latitude = request.POST['latitude']
+        longitude = request.POST['longitude']
+
+        # Create and save new location
+        Location.objects.create(
+            name=name,
+            address=address,
+            latitude=latitude,
+            longitude=longitude
+        )
+        return redirect('map:map')  # Redirect to map view after saving
+
+    return render(request, 'add_location.html')
